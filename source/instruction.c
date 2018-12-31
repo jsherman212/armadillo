6e964ad345934340955f1202654e0f3be5b97c89
Justin Sherman <justinsherman419@yahoo.com>
2018-12-30 21:23:55 -0500

    Forgot to add these

---
+++ source/instruction.c
@@ -0,0 +1,15 @@
+#include "instruction.h"
+#include <stdlib.h>
+
+struct instruction *instruction_new(unsigned int hex, unsigned long PC){
+	struct instruction *i = malloc(sizeof(struct instruction));
+
+	i->hex = hex;
+	i->PC = PC;
+
+	return i;
+}
+
+void instruction_free(struct instruction *i){
+	free(i);
+}
---
+++ source/instruction.h
@@ -0,0 +1,12 @@
+#ifndef _INSTRUCTION_H_
+#define _INSTRUCTION_H_
+
+struct instruction {
+	unsigned int hex;
+	unsigned long PC;
+};
+
+struct instruction *instruction_new(unsigned int, unsigned long);
+void instruction_free(struct instruction *);
+
+#endif
