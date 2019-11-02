7faa4472a349bf16941a03b838eac19380ca0ada
jsherman212 <justinsherman419@yahoo.com>
2019-11-02 17:57:42 -0400

    Advanced SIMD load/store single structure (post-indexed)?

--- source/LoadsAndStores.c
+++ source/LoadsAndStores.c
@@ -39,7 +39,7 @@ static int DisassembleLoadStoreMultStructuresInstr(struct instruction *i,
     ADD_FIELD(out, Q);
     ADD_FIELD(out, L);
 
-    if(!postidxed)
+    if(postidxed)
         ADD_FIELD(out, Rm);
 
     ADD_FIELD(out, opcode);
@@ -47,13 +47,13 @@ static int DisassembleLoadStoreMultStructuresInstr(struct instruction *i,
     ADD_FIELD(out, Rn);
     ADD_FIELD(out, Rt);
 
-    char *instr_s = NULL;
+    const char *instr_s = NULL;
     int instr_id = NONE;
 
     if(L == 0)
-        concat(&instr_s, "st");
+        instr_s = "st";
     else
-        concat(&instr_s, "ld");
+        instr_s = "ld";
 
     unsigned regcnt, selem;
 
@@ -74,12 +74,7 @@ static int DisassembleLoadStoreMultStructuresInstr(struct instruction *i,
         /* the way the AD_INSTR_* enum is set up makes this more complicated */
         instr_id = (AD_INSTR_LD1 - 1) + ((selem * 2) - 1);
 
-    concat(&instr_s, "%d", selem);
-
-    concat(&DECODE_STR(out), "%s { ", instr_s);
-
-    free(instr_s);
-    instr_s = NULL;
+    concat(&DECODE_STR(out), "%s%d { ", instr_s, selem);
 
     for(int i=Rt; i<(Rt+regcnt)-1; i++){
         ADD_REG_OPERAND(out, i, _SZ(_128_BIT), NO_PREFER_ZR, _SYSREG(NONE), _RTBL(AD_RTBL_FP_V_128));
@@ -88,7 +83,8 @@ static int DisassembleLoadStoreMultStructuresInstr(struct instruction *i,
         concat(&DECODE_STR(out), "%s.%s, ", Ri_s, T);
     }
 
-    ADD_REG_OPERAND(out, (Rt+regcnt)-1, _SZ(_128_BIT), NO_PREFER_ZR, _SYSREG(NONE), _RTBL(AD_RTBL_FP_V_128));
+    ADD_REG_OPERAND(out, (Rt+regcnt)-1, _SZ(_128_BIT), NO_PREFER_ZR, _SYSREG(NONE),
+            _RTBL(AD_RTBL_FP_V_128));
     const char *last_Rt_s = GET_FP_REG(AD_RTBL_FP_V_128, (Rt+regcnt)-1);
 
     ADD_REG_OPERAND(out, Rn, _SZ(_64_BIT), NO_PREFER_ZR, _SYSREG(NONE), _RTBL(AD_RTBL_GEN_64));
@@ -98,7 +94,8 @@ static int DisassembleLoadStoreMultStructuresInstr(struct instruction *i,
 
     if(postidxed){
         if(Rm != 0x1f){
-            ADD_REG_OPERAND(out, Rm, _SZ(_64_BIT), NO_PREFER_ZR, _SYSREG(NONE), _RTBL(AD_RTBL_GEN_64));
+            ADD_REG_OPERAND(out, Rm, _SZ(_64_BIT), NO_PREFER_ZR, _SYSREG(NONE),
+                    _RTBL(AD_RTBL_GEN_64));
             const char *Rm_s = GET_GEN_REG(AD_RTBL_GEN_64, Rm, NO_PREFER_ZR);
 
             concat(&DECODE_STR(out), ", %s", Rm_s);
@@ -121,167 +118,188 @@ static int DisassembleLoadStoreMultStructuresInstr(struct instruction *i,
     return 0;
 }
 
-/*
-char *DisassembleLoadStoreSingleStructuresInstr(struct instruction *instruction, int postidx){
-    char *disassembled = NULL;
+static int DisassembleLoadStoreSingleStructuresInstr(struct instruction *i,
+        struct ad_insn *out, int postidxed){
+    unsigned Q = bits(i->opcode, 30, 30);
+    unsigned L = bits(i->opcode, 22, 22);
+    unsigned R = bits(i->opcode, 21, 21);
+    unsigned Rm = bits(i->opcode, 16, 20);
+    unsigned opcode = bits(i->opcode, 13, 15);
+    unsigned S = bits(i->opcode, 12, 12);
+    unsigned size = bits(i->opcode, 10, 11);
+    unsigned Rn = bits(i->opcode, 5, 9);
+    unsigned Rt = bits(i->opcode, 0, 4);
 
-    unsigned int Rt = getbitsinrange(instruction->opcode, 0, 5);
-    unsigned int Rn = getbitsinrange(instruction->opcode, 5, 5);
-    unsigned int size = getbitsinrange(instruction->opcode, 10, 2);
-    unsigned int S = getbitsinrange(instruction->opcode, 12, 1);
-    unsigned int opcode = getbitsinrange(instruction->opcode, 13, 3);
-    unsigned int Rm = getbitsinrange(instruction->opcode, 16, 5);
-    unsigned int R = getbitsinrange(instruction->opcode, 21, 1);
-    unsigned int L = getbitsinrange(instruction->opcode, 22, 1);
-    unsigned int Q = getbitsinrange(instruction->opcode, 30, 1);
+    ADD_FIELD(out, Q);
+    ADD_FIELD(out, L);
+    ADD_FIELD(out, R);
 
-    int scale = getbitsinrange(opcode, 1, 2);
-    int selem = (((opcode & 1) << 1) | R) + 1;
+    if(postidxed)
+        ADD_FIELD(out, Rm);
 
-    char *instr = NULL;
+    ADD_FIELD(out, opcode);
+    ADD_FIELD(out, S);
+    ADD_FIELD(out, size);
+    ADD_FIELD(out, Rn);
+    ADD_FIELD(out, Rt);
+
+    const char *instr_s = NULL;
+    int instr_id = NONE;
+
+    if(L == 0)
+        instr_s = "st";
+    else
+        instr_s = "ld";
+
     const char *suffix = NULL;
 
-    int index = 0;
+    unsigned scale = opcode >> 1;
+    unsigned selem = (((opcode & 1) << 1) | R) + 1;
+    unsigned index = 0;
+
+    int replicate = 0;
 
     switch(scale){
-        case 3:
+        case 3: replicate = 1; break;
+        case 0:
             {
-                // load and replicate
-                if(L == 0 && S == 1){
-                    free(instr);
-                    return strdup(".undefined");
-                }
-
-                scale = size;
-
-                if(!instr){
-                    instr = malloc(8);
-                    sprintf(instr, "ld%dr", selem);
-                }
-
+                index = (Q << 3) | (S << 2) | size;
+                suffix = "b";
                 break;
             }
-        case 0: // B[0-15]
-            index = (Q << 3) | (S << 2) | size;
-            suffix = "b";
-            //printf("index: %d\n", index);
-            break;
-        case 1: // H[0-7]
-            if((size & 1) == 1)
-                return strdup(".undefined");
-
-            index = (Q << 2) | (S << 1) | (size >> 1);
-            suffix = "h";
-            break;
+        case 1:
+            {
+                index = (Q << 2) | (S << 1) | (size >> 1);
+                suffix = "h";
+                break;
+            }
         case 2:
             {
-                if(((size << 1) & 1) == 1)
-                    return strdup(".undefined");
-
-                // S[0-3]
                 if((size & 1) == 0){
                     index = (Q << 1) | S;
                     suffix = "s";
                 }
-                // D[0-1]
                 else{
-                    if(S == 1)
-                        return strdup(".undefined");
-
                     index = Q;
                     suffix = "d";
-                    scale = 3;
                 }
 
                 break;
             }
+        default: return 1;
     };
 
-    disassembled = malloc(256);
+    if(replicate)
+        instr_id = (AD_INSTR_LD1R - 1) + ((selem * 2) - 1);
+    else if(L == 0)
+        instr_id = (AD_INSTR_ST1 - 1) + selem;
+    else if(L == 1)
+        instr_id = (AD_INSTR_LD1 - 1) + ((selem * 2) - 1);
 
-    // we can get the post index immediate
-    // by multipling whatever index
-    // corresponds with our suffix by selem
-    int ldstimms[] = {1, 2, 4, 8};
+    concat(&DECODE_STR(out), "%s%d%s { ", instr_s, selem, replicate ? "r" : "");
 
-    // figure out instruction type
-    // if it hasn't been initialized yet, it's not
-    // a load and replicate
-    if(!instr){
-        instr = malloc(8);
+    for(int i=Rt; i<(Rt+selem)-1; i++){
+        ADD_REG_OPERAND(out, i, _SZ(_128_BIT), NO_PREFER_ZR, _SYSREG(NONE),
+                _RTBL(AD_RTBL_FP_V_128));
+        const char *Ri_s = GET_FP_REG(AD_RTBL_FP_V_128, i);
 
-        if(L == 0)
-            sprintf(instr, "st%d", selem);
-        else
-            sprintf(instr, "ld%d", selem);
+        concat(&DECODE_STR(out), "%s", Ri_s);
 
-        sprintf(disassembled, "%s {", instr);
-        free(instr);
+        if(replicate){
+            const char *T = get_arrangement(size, Q);
 
-        for(int i=Rt; i<(Rt+selem); i++)
-            sprintf(disassembled, "%s%s.%s, ", disassembled, ARM64_VectorRegisters[i], suffix);
+            if(!T)
+                return 1;
 
-        // remove the extra space at the end
-        disassembled[strlen(disassembled) - 2] = '\0';
+            concat(&DECODE_STR(out), ".%s", T);
+        }
+        else{
+            concat(&DECODE_STR(out), ".%s", suffix);
+        }
 
-        const char *Xn = Rn == 31 ? "sp" : ARM64_GeneralRegisters[Rn];
+        concat(&DECODE_STR(out), ", ");
+    }
 
-        // build the rest of the instruction
-        sprintf(disassembled, "%s}[%d], [%s]", disassembled, index, Xn);
+    ADD_REG_OPERAND(out, (Rt+selem)-1, _SZ(_128_BIT), NO_PREFER_ZR, _SYSREG(NONE),
+            _RTBL(AD_RTBL_FP_V_128));
+    const char *last_Rt_s = GET_FP_REG(AD_RTBL_FP_V_128, (Rt+selem)-1);
 
-        // check for any post-index stuff
-        if(postidx){
-            // if Rm is not 0x1f, we have a post index register
-            if(Rm != 0x1f)
-                sprintf(disassembled, "%s, %s", disassembled, ARM64_GeneralRegisters[Rm]);
+    concat(&DECODE_STR(out), "%s", last_Rt_s);
+
+    if(replicate){
+        const char *T = get_arrangement(size, Q);
+
+        if(!T)
+            return 1;
+
+        concat(&DECODE_STR(out), ".%s", T);
+    }
+    else{
+        concat(&DECODE_STR(out), ".%s", suffix);
+    }
+
+    concat(&DECODE_STR(out), " }");
+
+    if(!replicate){
+        ADD_IMM_OPERAND(out, AD_UINT, *(unsigned int *)&index);
+        concat(&DECODE_STR(out), "[%d]", index);
+    }
+
+    ADD_REG_OPERAND(out, Rn, _SZ(_64_BIT), NO_PREFER_ZR, _SYSREG(NONE), _RTBL(AD_RTBL_GEN_64));
+    const char *Rn_s = GET_GEN_REG(AD_RTBL_GEN_64, Rn, NO_PREFER_ZR);
+
+    concat(&DECODE_STR(out), ", [%s]", Rn_s);
+
+    int rimms[] = { 1, 2, 4, 8 };
+
+    if(postidxed){
+        if(replicate){
+            if(Rm != 0x1f){
+                ADD_REG_OPERAND(out, Rm, _SZ(_64_BIT), NO_PREFER_ZR, _SYSREG(NONE),
+                        _RTBL(AD_RTBL_GEN_64));
+                const char *Rm_s = GET_GEN_REG(AD_RTBL_GEN_64, Rm, NO_PREFER_ZR);
+
+                concat(&DECODE_STR(out), ", %s", Rm_s);
+            }
             else{
-                // assume we have an 8 bit varient
-                int immidx = 0;
+                unsigned imm = rimms[selem] * selem;
+                ADD_IMM_OPERAND(out, AD_UINT, *(unsigned int *)&imm);
 
-                if(strcmp(suffix, "h") == 0)
-                    immidx = 1;
-                else if(strcmp(suffix, "s") == 0)
-                    immidx = 2;
-                else if(strcmp(suffix, "d") == 0)
-                    immidx = 3;
+                concat(&DECODE_STR(out), ", #%#x", imm);
+            }
 
-                sprintf(disassembled, "%s, #%d", disassembled, ldstimms[immidx] * selem);
+        }
+        else{
+            if(Rm != 0x1f){
+                ADD_REG_OPERAND(out, Rm, _SZ(_64_BIT), NO_PREFER_ZR, _SYSREG(NONE),
+                        _RTBL(AD_RTBL_GEN_64));
+                const char *Rm_s = GET_GEN_REG(AD_RTBL_GEN_64, Rm, NO_PREFER_ZR);
+
+                concat(&DECODE_STR(out), ", %s", Rm_s);
+            }
+            else{
+                int idx = 0;
+
+                if(*suffix == 'h')
+                    idx = 1;
+                else if(*suffix == 's')
+                    idx = 2;
+                else if(*suffix == 'd')
+                    idx = 3;
+
+                unsigned imm = rimms[idx] * selem;
+                ADD_IMM_OPERAND(out, AD_UINT, *(unsigned int *)&imm);
+
+                concat(&DECODE_STR(out), ", #%#x", imm);
             }
         }
     }
-    // it's a load and replicate
-    else{
-        const char *T = get_arrangement(size, Q);
 
-        if(!T){
-            free(disassembled);
-            free(instr);
-            return strdup(".undefined");
-        }
+    SET_INSTR_ID(out, instr_id);
 
-        sprintf(disassembled, "%s {", instr);
-        free(instr);
-
-        for(int i=Rt; i<(Rt+selem); i++)
-            sprintf(disassembled, "%s%s.%s, ", disassembled, ARM64_VectorRegisters[i], T);
-
-        disassembled[strlen(disassembled) - 2] = '\0';
-
-        const char *Xn = Rn == 31 ? "sp" : ARM64_GeneralRegisters[Rn];
-
-        sprintf(disassembled, "%s}, [%s]", disassembled, Xn);
-
-        if(postidx){
-            if(Rm != 0x1f)
-                sprintf(disassembled, "%s, %s", disassembled, ARM64_GeneralRegisters[Rm]);
-            else
-                sprintf(disassembled, "%s, #%d", disassembled, ldstimms[selem] * selem);
-        }
-    }
-
-    return disassembled;
+    return 0;
 }
-
+/*
 char *DisassembleLoadAndStoreExclusiveInstr(struct instruction *instruction){
     char *disassembled = NULL;
 
@@ -1220,9 +1238,10 @@ int LoadsAndStoresDisassemble(struct instruction *i, struct ad_insn *out){
     unsigned op3 = bits(i->opcode, 16, 21);
     unsigned op4 = bits(i->opcode, 10, 11);
 
-    if((op0 & ~4) == 0 && (op2 == 0 || op2 == 1) && (op3 >> 5) == 0){
+    if((op0 & ~4) == 0 && (op2 == 0 || op2 == 1) && (op3 >> 5) == 0)
         result = DisassembleLoadStoreMultStructuresInstr(i, out, op2);
-    }
+    else if((op0 & ~4) == 0 && (op2 == 2 || op2 == 3))
+        result = DisassembleLoadStoreSingleStructuresInstr(i, out, op2 != 2);
     else{
         result = 1;
     }
